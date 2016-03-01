6a532ad81cb700f1f0e2eebc7c5cf31f2aae3516
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-03-01 18:44:05 +0200

    added test for helpers

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
+  test "Visit Signup page" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 end
