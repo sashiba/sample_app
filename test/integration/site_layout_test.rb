a9545bd804a0fce7d97827bf3db1984aa4b930a8
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-03-08 16:55:15 +0200

    update, delete, index users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -10,6 +13,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get signup_path
     assert_select "title", full_title("Sign up")
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
   end
 
   test "Visit Signup page" do
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -49,4 +49,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name,  @user.name
     assert_equal email, @user.email
   end
+
+  test "friendly forwarding only on first login" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    assert_equal session[:forwarding_url], nil
+  end
 end
