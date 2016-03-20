a14728f4d9e738f96a9f0aabe5bec0e4bc3d9987
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-03-20 19:19:52 +0200

    add little implementaion for following/ers

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ActiveRecord::Base
 
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 250 },
@@ -15,6 +24,18 @@ class User < ActiveRecord::Base
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
   # Returns the hash digest of the given string.
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name: "Example User",
-             email: "example@railstutorial.com",
+             email: "example@railstutorial.org",
              password: "foobar",
              password_confirmation: "foobar",
              admin: true,
@@ -30,3 +30,10 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
